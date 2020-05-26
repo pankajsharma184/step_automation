f65a5b4ca057eb11bc126bbde384e5bc898e4f78
aks <aks@BMI0327.bytemanagers.com>
2020-05-26 18:28:52 +0530

    AKSH : get sample files skeleton

---
+++ src/com/codifyd/automation/attribute/AttributeSampleInfo.java
@@ -0,0 +1,19 @@
+package com.codifyd.automation.attribute;
+
+import java.io.File;
+
+public class AttributeSampleInfo {
+	
+	public File getSamplePropertiesFile(){
+		return null;
+	}
+	
+	public File getSampleInputExcelFile(){
+		return null;
+	}
+	
+	public File getSampleInputXMLFile(){
+		return null;
+	}
+	
+}
