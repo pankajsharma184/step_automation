c742a1b40b82f0a65fbe2f32c5d253f8db7dfd4c
ATripathi <atripathi@codifyd.com>
2020-05-26 20:15:11 +0530

    ANUJT: Sample Input File Handler

--- src/com/codifyd/automation/attribute/AttributeSampleInfo.java
+++ src/com/codifyd/automation/attribute/AttributeSampleInfo.java
@@ -1,19 +1,43 @@
 package com.codifyd.automation.attribute;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+
+import com.codifyd.automation.util.SampleFileHelperUtil;
 
 public class AttributeSampleInfo {
-	
-	public File getSamplePropertiesFile(){
-		return null;
+
+	public File getSamplePropertiesFile() throws IOException {
+		InputStream inStream = SampleFileHelperUtil.getSamplePropertiesFile(SampleFileHelperUtil.ATTRIBUTE);
+		URI outputUri = Paths.get(System.getProperty("java.io.tmpdir"), "AttributeConfigInputSample.properties")
+				.toUri();
+		File outputFile = new File(outputUri);
+		FileUtils.copyToFile(inStream, outputFile);
+
+		return outputFile;
 	}
-	
-	public File getSampleInputExcelFile(){
-		return null;
+
+	public File getSampleInputExcelFile() throws IOException {
+		InputStream inStream = SampleFileHelperUtil.getSampleExcelFile(SampleFileHelperUtil.ATTRIBUTE);
+		URI outputUri = Paths.get(System.getProperty("java.io.tmpdir"), "AttributeExcelSampleInput.xlsx").toUri();
+		File outputFile = new File(outputUri);
+		FileUtils.copyToFile(inStream, outputFile);
+
+		return outputFile;
 	}
-	
-	public File getSampleInputXMLFile(){
-		return null;
+
+	public File getSampleInputXMLFile() throws IOException {
+		InputStream inStream = SampleFileHelperUtil.getSampleXMLFile(SampleFileHelperUtil.ATTRIBUTE);
+		URI outputUri = Paths.get(System.getProperty("java.io.tmpdir"), "AttributeXMLInputSample.xml").toUri();
+		File outputFile = new File(outputUri);
+		FileUtils.copyToFile(inStream, outputFile);
+
+		return outputFile;
 	}
-	
+
 }
---
+++ src/com/codifyd/automation/util/SampleFileHelperUtil.java
@@ -0,0 +1,66 @@
+package com.codifyd.automation.util;
+
+import java.io.InputStream;
+
+public class SampleFileHelperUtil {
+	public static final String ATTRIBUTE = "Attribute";
+	public static final String ATTRIBUTELINK = "Attributelink";
+	public static final String LOV = "LOV";
+	public static final String UOM = "UOM";
+	public static final String TAXONOMY = "Taxonomy";
+
+	public static InputStream getSamplePropertiesFile(String str) {
+		InputStream inStream = null;
+		ClassLoader classLoader = SampleFileHelperUtil.class.getClassLoader();
+		if (str.equals(ATTRIBUTE)) {
+			inStream = classLoader.getResourceAsStream("resources/Attribute-Config.properties");
+		} else if (str.equals(ATTRIBUTELINK)) {
+			inStream = classLoader.getResourceAsStream("resources/AttributeLink-Config.properties");
+		} else if (str.equals(LOV)) {
+			inStream = classLoader.getResourceAsStream("resources/LOV-Config.properties");
+		} else if (str.equals(UOM)) {
+			inStream = classLoader.getResourceAsStream("resources/UOM-Config.properties");
+		} else if (str.equals(TAXONOMY)) {
+			inStream = classLoader.getResourceAsStream("resources/Taxonomy-Config.properties");
+		}
+
+		return inStream;
+	}
+
+	public static InputStream getSampleXMLFile(String str) {
+		InputStream inStream = null;
+		ClassLoader classLoader = SampleFileHelperUtil.class.getClassLoader();
+		if (str.equals(ATTRIBUTE)) {
+			inStream = classLoader.getResourceAsStream("sample/AttributeXMLInputSample.xml");
+		} else if (str.equals(ATTRIBUTELINK)) {
+			inStream = classLoader.getResourceAsStream("sample/AttributeLinkXMLInputSample.xml");
+		} else if (str.equals(LOV)) {
+			inStream = classLoader.getResourceAsStream("sample/LOVXMLInputSample.xml");
+		} else if (str.equals(UOM)) {
+			inStream = classLoader.getResourceAsStream("sample/UnitsOfMeasureXMLInputSample.xml");
+		} else if (str.equals(TAXONOMY)) {
+			inStream = classLoader.getResourceAsStream("sample/TaxonomyXMLInputSample.xml");
+		}
+
+		return inStream;
+	}
+
+	public static InputStream getSampleExcelFile(String str) {
+		InputStream inStream = null;
+		ClassLoader classLoader = SampleFileHelperUtil.class.getClassLoader();
+		if (str.equals(ATTRIBUTE)) {
+			inStream = classLoader.getResourceAsStream("sample/AttributeXMLInputSample.xlsx");
+		} else if (str.equals(ATTRIBUTELINK)) {
+			inStream = classLoader.getResourceAsStream("sample/AttributeLinkXMLInputSample.xlsx");
+		} else if (str.equals(LOV)) {
+			inStream = classLoader.getResourceAsStream("sample/LOVXMLInputSample.xml");
+		} else if (str.equals(UOM)) {
+			inStream = classLoader.getResourceAsStream("sample/UnitsOfMeasureXMLInputSample.xlsx");
+		} else if (str.equals(TAXONOMY)) {
+			inStream = classLoader.getResourceAsStream("sample/TaxonomyXMLInputSample.xlsx");
+		}
+		return inStream;
+
+	}
+
+}
