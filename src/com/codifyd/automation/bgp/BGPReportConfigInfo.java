d2bf50a2a186cc83e461cac83dc156096902f86c
aks <aks@BMI0327.bytemanagers.com>
2020-06-04 11:59:21 +0530

    AKSH : adding mil conversion and sample file info data

---
+++ src/com/codifyd/automation/bgp/BGPReportConfigInfo.java
@@ -0,0 +1,25 @@
+package com.codifyd.automation.bgp;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+
+import com.codifyd.automation.stepconversion.util.AutomationConstants;
+import com.codifyd.automation.stepconversion.util.SampleFileHelperUtil;
+
+public class BGPReportConfigInfo {
+
+	public File getSampleInputFile() throws IOException {
+		InputStream inStream = SampleFileHelperUtil.getSampleInputFile(AutomationConstants.BGP);
+		URI outputUri = Paths.get(System.getProperty("java.io.tmpdir"), "BGPReportInput.txt")
+				.toUri();
+		File outputFile = new File(outputUri);
+		FileUtils.copyToFile(inStream, outputFile);
+
+		return outputFile;
+	}
+}
